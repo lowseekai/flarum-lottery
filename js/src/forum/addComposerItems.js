27bcae061af34e9c9aacc18be0f4bb50fd7a5da8
gungun88 <pengqibao3@gmail.com>
2026-09-13 09:06:57 +0800

    Fix lottery composer initialization on Flarum 2

--- js/src/forum/addComposerItems.js
+++ js/src/forum/addComposerItems.js
@@ -2,28 +2,31 @@ import app from 'flarum/forum/app';
 
 import { extend } from 'flarum/common/extend';
 import classList from 'flarum/common/utils/classList';
-import DiscussionComposer from 'flarum/forum/components/DiscussionComposer';
 
 import CreateLotteryModal from './components/CreateLotteryModal';
 
-export const addToComposer = (composer) => {
-  composer.prototype.addLottery = function () {
-    app.modal.show(CreateLotteryModal, {
-      lottery: this.composer.fields.lottery,
-      onsubmit: (lottery) => {
-        this.composer.fields.lottery = lottery;
-      },
-    });
-  };
+export const addToComposer = (path) => {
+  // Flarum 2 lazy-loads composer components, so resolve the module through
+  // the extender instead of reading its prototype during boot.
+  extend(path, 'oninit', function () {
+    this.addLottery = () => {
+      app.modal.show(CreateLotteryModal, {
+        lottery: this.composer.fields.lottery,
+        onsubmit: (lottery) => {
+          this.composer.fields.lottery = lottery;
+        },
+      });
+    };
+  });
 
-  extend(composer.prototype, 'headerItems', function (items) {
+  extend(path, 'headerItems', function (items) {
     const discussion = this.composer.body?.attrs?.discussion;
     const canStartLottery = discussion?.canStartLottery() ?? app.forum.canStartLottery();
 
     if (canStartLottery) {
       items.add(
         'lottery',
-        <a className="ComposerBody-lottery" onclick={this.addLottery.bind(this)}>
+        <a className="ComposerBody-lottery" onclick={this.addLottery}>
           <span className={classList('LotteryLabel', !this.composer.fields.lottery && 'none')}>
             {app.translator.trans(
               `nodeloc-lottery.forum.composer_discussion.${
@@ -37,7 +40,7 @@ export const addToComposer = (composer) => {
     }
   });
 
-  extend(composer.prototype, 'data', function (data) {
+  extend(path, 'data', function (data) {
     if (this.composer.fields.lottery) {
       data.lotteryData = this.composer.fields.lottery;
     }
@@ -45,5 +48,5 @@ export const addToComposer = (composer) => {
 };
 
 export default () => {
-  addToComposer(DiscussionComposer);
+  addToComposer('flarum/forum/components/DiscussionComposer');
 };
