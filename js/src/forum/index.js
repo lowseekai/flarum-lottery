6f71311959116a70d92f27b2fe23f7b4eaceda04
gungun88 <pengqibao3@gmail.com>
2026-09-14 04:20:58 +0800

    Fix lottery rendering without duplicate model registration

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -6,13 +6,11 @@ import addLotteryToPost from './addLotteryToPost';
 import FailLotteryNotification from './components/FailLotteryNotification';
 import FinishLotteryNotification from './components/FinishLotteryNotification';
 import DrawLotteryNotification from './components/DrawLotteryNotification';
-import extend from './extend';
 
 export * from './components';
 export * from './models';
 
 app.initializers.add('nodeloc/lottery', () => {
-  extend.forEach((extender) => extender.extend(app));
   addDiscussionBadge();
   addComposerItems();
   addLotteryToPost();
@@ -21,4 +19,4 @@ app.initializers.add('nodeloc/lottery', () => {
   app.notificationComponents.finishLottery = FinishLotteryNotification;
 });
 
-export { extend };
+export { default as extend } from './extend';
